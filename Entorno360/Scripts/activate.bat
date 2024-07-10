bfca2a2aa4e524d523ae75bdf94da7bca8341926
luis <luif.sepulveda@duocuc.cl>
2024-07-10 17:49:01 -0400

    cambios

--- Entorno360/Scripts/activate.bat
+++ Entorno360/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\usario\Desktop\ProjectDjango\Entorno360
+set VIRTUAL_ENV=C:\Campus360\Entorno360
 
 if not defined PROMPT set PROMPT=$P$G
 
